d8161768fb670b95a560e7e4eb819c19a2f89ab2
Lu-Vuong <lu-vuongle@hotmail.com>
2017-06-29 22:04:09 +1000

    Added startGuess() logic -User enters a guess, if correct display a letter -If wrong read from file and display body part based on number of tries left

--- Hangman_Console/Hangman_Console.cpp
+++ Hangman_Console/Hangman_Console.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 void readFile(string filename);
 void startGame();
+void startGuess(string playerName, string assignedWord, string hidden, int failed_inputs, int char_exposed);
 
 int main()
 {
@@ -65,7 +66,111 @@ void startGame() {
 		display[i] = '*';
 	}
 
-	//startGuess(player_name, assignedWord, display, failed_inputs, char_exposed);
+	startGuess(player_name, assignedWord, display, failed_inputs, char_exposed);
+}
+
+void startGuess(string playerName, string word, string hidden, int failed_inputs, int char_exposed)
+{
+	//Identify a list of max guesses = 5
+	string guess;
+	string start_new_game;
+
+	while (char_exposed < word.length()) {
+		bool correct_guess = false;
+		//Begin Letting player guess the character one by one
+		cout << "\n\nPlease enter a character to guess in: " << hidden;
+		cout << "\nEnter Letter: ";
+
+		cin >> guess;
+
+		if (guess.length() == 1) {
+			//If guess was correct, reveal a letter in the array
+			for (int i = 0; i < word.length(); i++) {
+				if (guess[0] == word[i]) {
+					if (hidden[i] == word[i]) {
+						cout << "\nYouve already guessed this letter!" << endl;
+						startGuess(playerName, word, hidden, failed_inputs, char_exposed);
+					}
+					else {
+						//If letter matches, display the hidden letter
+						hidden[i] = word[i];
+						//Logic to display one less star if character guessed is correct
+						cout << ">>> You guessed right! {" << word[i] << "}";
+						char_exposed++;
+						correct_guess = true;
+						continue;
+					}
+				}
+			}
+		}
+		else {
+			cout << "Please enter one letter only" << endl;
+			startGuess(playerName, word, hidden, failed_inputs, char_exposed);
+		}
+
+		if (correct_guess == false) {
+			failed_inputs--;
+			switch (failed_inputs) {
+			case 0:
+				readFile("gameOver.txt");
+				cout << "You have run out of tries" << endl;
+				cout << "The word was: " << word << endl;
+				cout << "Better luck next time!" << endl;
+				cout << "\nWould you like to play again? [Y/N]" << endl;
+				cin >> start_new_game;
+				if (start_new_game == "Y" || start_new_game == "y") {
+					startGame();
+				}
+				else if (start_new_game == "N" || start_new_game == "n") {
+					cout << "See you next time!!" << endl;
+					exit(EXIT_SUCCESS);
+				}
+				break;
+			case 1:
+				readFile("showRLeg.txt");
+				cout << "\nYou have one more chance left!!\n" << endl;
+				break;
+			case 2:
+				readFile("showLLeg.txt");
+				cout << "You have: " << failed_inputs << " tries left" << endl;
+				break;
+			case 3:
+				readFile("showRArm.txt");
+				cout << "You have: " << failed_inputs << " tries left" << endl;
+				break;
+			case 4:
+				readFile("showLArm.txt");
+				cout << "You have: " << failed_inputs << " tries left" << endl;
+				break;
+			case 5:
+				readFile("showBody.txt");
+				cout << "You have: " << failed_inputs << " tries left" << endl;
+				break;
+			case 6:
+				readFile("showHead.txt");
+				cout << "You have: " << failed_inputs << " tries left" << endl;
+				break;
+			default:
+				cout << "Error" << endl;
+				break;
+			}
+		}
+	}
+
+	if (char_exposed == hidden.length()) {
+		cout << "\nThe word was: " << hidden << endl;
+		cout << "\nCongratulations: " << playerName << endl;
+		readFile("win.txt");
+		cout << "\nWould you like to play again? [Y/N]";
+		cin >> start_new_game;
+		if (start_new_game == "Y" || start_new_game == "y") {
+			startGame();
+		}
+		else {
+			cout << "See you next time!!" << endl;
+			exit(EXIT_SUCCESS);
+		}
+	}
 }
 
 void readFile(string filename) {
